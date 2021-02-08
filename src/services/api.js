db0656768b1c76d19aa517d867ba38a680dc7e12
David Levy Sousa Oliveira <jbdeivin@gmail.com>
2021-02-07 22:39:44 -0300

    add functions

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://0.0.0.0:8000",
-  // baseURL: "http://versionator-api.herokuapp.com",
+  baseURL: "http://versionator-api.herokuapp.com",
   Accept: "application/json",
   "Content-type": "multipart/form-data",
 });
